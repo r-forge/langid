7a7d93ce38a917c91c09d94186598380333cb481
feinerer (Ingo Feinerer) <feinerer@logic.at>
2009-06-25 18:13:14 +0000

    tmReduce is already in tm.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/langid@3 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/modify_icorpus.R
+++ pkg/R/modify_icorpus.R
@@ -3,11 +3,8 @@ setMethod("makeUTF",
           signature(object = "PlainTextDocument"),
           function(object, ...) {
               Encoding(Content(object)) <- "UTF-8"
-              return(object)
+              object
           })
-
-tmReduce <- function(x, tmFuns, ...)
-    Reduce(function(f, ...) f(...), tmFuns, x, right = TRUE)
 
 modify_icorpus <- function(icorpus)
     tmMap(icorpus, FUN=tmReduce,
